53db2975bfe905bff10cf90e689717add7e9377f
Odysseas Diamadis <odysseas@dilmun.net>
2025-07-13 18:57:59 +0200

    crossplatform

--- src/test-gtts.py
+++ src/test-gtts.py
@@ -4,6 +4,11 @@ import os
 # text = "This is a test of Google Text-to-Speech on Linux."
 # language = 'en'
 
+USE_PULSE = False
+ps = os.environ.get("PULSE_SERVER")
+if ps and ps.startswith("unix:"):
+    USE_PULSE = True
+
 text = "Ciao, il mio nome è Tiago!"
 language = 'it'
 
@@ -12,4 +17,5 @@ os.makedirs("audio_output", exist_ok=True)
 tts.save("audio_output/test_output.mp3")
 
 # Play using mpg123
-os.system("mpg123 audio_output/test_output.mp3")
+cmd = "mpg123" if not USE_PULSE else "mpg-pulse"
+os.system(f"{cmd} audio_output/test_output.mp3")
