99f8a963ae11fd6b9d4b2ab2eb650335a47512f0
gionnidamico <damico.giovanni.1998@gmail.com>
2025-09-12 23:27:41 +0200

    added config files for working with a tiago_env, both in venv and in conda

--- commands.md
+++ commands.md
@@ -11,6 +11,15 @@ source /tiago_public_ws/devel/setup.bash
 source /src/tiago_ws/devel/setup.bash
 ```
 
+Source venv environment after it is created
+```sh
+# note: should launch the ./install-deps.sh first
+source /src/tiago_ws/source.bash
+```
+
+
+```
+
 Launch gazebo
 ```sh
 roslaunch tiago_gazebo tiago_gazebo.launch public_sim:=true end_effector:=pal-hey5 world:=dnd
--- src/install-deps.sh
+++ src/install-deps.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
 # This script installs the required dependancies and libs required in this project.
 # Run it after starting this docker and after calling ./join.sh.
 
+# REMEMBER: you should launch this script from root
+if [ "$PWD" != "/" ]; then
+    echo "We are NOT in the root directory. Moving into: /"
+    cd /
+fi
+
+# update system
 sudo apt update --yes
+sudo apt install python3-venv --yes
 
 # libs for audio I/O
 sudo apt install mpg123 --yes
@@ -11,18 +19,47 @@ sudo apt-get install portaudio19-dev --yes
 sudo apt install -y alsa-utils
 sudo apt-get install -y libasound2-plugins
 
+
+# Define the virtual environment directory
+VENV_DIR="src/venv/"
+REQUIREMENTS_FILE="src/config/tiago_requirements.txt"
+
+# Check if the directory exists
+if [ -d "$VENV_DIR" ]; then
+    echo "Virtual environment found at ./$VENV_DIR"
+else
+    echo "Creating virtual environment..."
+    python3 -m venv "$VENV_DIR"
+    source "$VENV_DIR/bin/activate"
+    pip install -r $REQUIREMENTS_FILE
+    echo "Virtual environment created at ./$VENV_DIR"
+fi
+
+# Activate the virtual environment - not useful for now
+# source "$VENV_DIR/bin/activate"
+
+
 # libs needed to use the models required for tiago brain
-pip install transformers gtts flask 
-pip install -U openai-whisper
-pip install pyaudio
+# pip install transformers gtts flask 
+# pip install -U openai-whisper
+# pip install pyaudio
 
 
-# pip install opencv-python tensorflow face_recognition numpy scipy
+# # pip install opencv-python tensorflow face_recognition numpy scipy
 
 
-# face detection
-#pip install face-recognition
-pip install tensorflow==2.11.0 deepface
-pip install ultralytics opencv-python
-pip install supervision pillow
-pip install deepface
+# # face detection
+# #pip install face-recognition
+# pip install tensorflow==2.11.0 deepface
+# pip install ultralytics opencv-python
+# pip install supervision pillow
+# pip install deepface
+
+
+
+# remember to: (try fixing drivers)
+# glxinfo | grep -i 'OpenGL renderer'
+# sudo add-apt-repository ppa:kisak/kisak-mesa
+# sudo apt update --fix-missing
+# sudo apt upgrade --fix-missing
+# sudo apt install mesa-utils --yes
